b4001ae78fc36605798ce3788545d26f12e33f8b
Matt Ludwigs <matt.ludwigs@knledg.com>
2017-04-13 07:47:49 -0700

    fix naming of appendable

---
+++ lib/containers/protocols/appendable.ex
@@ -0,0 +1,12 @@
+defprotocol Containers.Appendable do
+  @moduledoc """
+  Protocol for appending things by exposing the `append` function
+  """
+
+  @doc """
+  Append takes two of the same Appendable value and puts them together. Returning
+  a new struct with the appended value.
+  """
+  @spec append(struct(), struct()) :: struct()
+  def append(v1, v2)
+end
