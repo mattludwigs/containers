cb8c8652ccfa53488e199a3b0c6dae3b6a1e42df
Matt Ludwigs <mattludwigs@gmail.com>
2017-10-27 08:31:50 -0700

    change flatten to join

--- lib/containers/protocols/joinable.ex
+++ lib/containers/protocols/joinable.ex
@@ -1,6 +1,6 @@
 defprotocol Containers.Joinable do
   @moduledoc """
-  Protocol for flattening container structures by exposing the `flatten` function
+  Protocol for joining nested container structures by exposing the `join` function
   """
   @spec join(Containers.Joinable)
   :: Containers.Joinable
